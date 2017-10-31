640298a21f97815880f0e01e20bbc58aa6461192
Aaron Fleshner <adfleshner@gmail.com>
2017-10-31 00:08:56 -0400

    Added High score Added game restart Updated Readme. Added new GAME interface to make code more readable.

---
+++ app/src/main/java/com/flesh/aaronsays/HighScoreUtils.kt
@@ -0,0 +1,24 @@
+package com.flesh.aaronsays
+
+import android.content.Context
+import android.preference.PreferenceManager
+
+/**
+ * Shared Prefs helper class for Score Keeping.
+ * Created by aaronfleshner on 10/30/17.
+ */
+class HighScoreUtils(context: Context) {
+
+    val KEY = "[${HighScoreUtils::class.java.canonicalName}] HIGH_SCORE"
+
+    val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+    val edit = prefs.edit()
+
+
+    fun setHighScore(highScore: Int){
+        edit.putInt(KEY,highScore).apply()
+    }
+
+    fun getHighScore() = prefs.getInt(KEY,0)
+
+}
